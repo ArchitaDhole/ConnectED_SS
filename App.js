a43b8c68b9d3ea1620b883f3e9e88728fef167fc
ArchitaDhole <mvdhole@gmail.com>
2023-06-01 20:50:26 +0530

    Updated1

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Blacklist">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={Home}
